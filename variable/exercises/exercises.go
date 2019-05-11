f94dd42da623764355c476c835aa0ea64ab51297
smalldog124 <panumars124@gmail.com>
2019-05-11 11:53:16 +0700

    [Created]:สร้าง exercises ของ variable

---
+++ variable/exercises/exercises.go
@@ -0,0 +1,18 @@
+package main
+
+// Add imports
+
+func main() {
+	// Declare variables that are set to their zero value.
+
+	// Display the value of those variables.
+
+	// Declare variables and initialize.
+	// Using the short variable declaration operator.
+
+	// Display the value of those variables.
+
+	// Perform a type conversion.
+
+	// Display the value of that variable.
+}
