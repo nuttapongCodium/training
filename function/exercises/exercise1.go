b72110926bbd1f5bc39454a289ee2b2e45e13f8a
platylm <n_nareenart@kkumail.com>
2019-05-11 16:57:00 +0700

    [Edited]: exercise ของ function

--- function/exercises/exercise1.go
+++ function/exercises/exercise1.go
@@ -1,22 +1,23 @@
+// Declare a struct type to maintain information about a user. Declare a function
+// that creates value of and returns pointers of this type and an error value. Call
+// this function from main and display the value.
+//
+// Make a second call to your function but this time ignore the value and just test
+// the error value.
 package main
 
 // Add imports.
 
-// Declare a type named user.
+// user represents a user in the system.
 
-// Declare a function that creates user type values and returns a pointer
-// to that value and an error value of nil.
-func funcName() /* (pointer return arg, error return arg) */ {
-
-	// Create a value of type user and return the proper values.
-}
+// newUser creates and returns pointers of user type values.
 
 func main() {
 
-	// Use the function to create a value of type user. Check
-	// the error being returned.
+	// Create a value of type user.
 
-	// Display the value that the pointer points to.
+	// Display the value.
 
-	// Call the function again and just check the error.
+	// Call the function and just check the error on the return.
+
 }
