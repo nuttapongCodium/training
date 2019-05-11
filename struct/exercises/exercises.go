846f9c25ab86e6d0427675af4155fd2a966f824a
smalldog124 <panumars124@gmail.com>
2019-05-11 15:33:07 +0700

    [Created]:สร้าง exercises ของ struct

---
+++ struct/exercises/exercises.go
@@ -0,0 +1,21 @@
+package main
+
+// Add imports
+
+func main() {
+
+	// Declare variable of type user and init using a struct literal.
+
+	// Display the field values.
+
+	// Declare a variable using an anonymous struct.
+
+	// Display the field values.
+
+	// Declare variable of type product public and init using a struct literal.
+	// set struct tag
+
+	// Call json.Mashal package encode/json
+
+	// Display the field values.
+}
