8044468f46239af2de7ed847736368199360bd58
platylm <n_nareenart@kkumail.com>
2019-05-11 16:02:08 +0700

    [Edited]: เปลี่ยนชื่อไฟล์และแก้โจทย์ของ method

---
+++ method/exercises/exercise1.go
@@ -0,0 +1,21 @@
+// Declare a struct that represents a baseball player. Include name, atBats and hits.
+// Declare a method that calculates a players batting average. The formula is hits / atBats.
+// Declare a slice of this type and initialize the slice with several players. Iterate over
+// the slice displaying the players name and batting average.
+package main
+
+// Add imports.
+
+// struct name player represents a person in the game.
+
+// average calculates the batting average for a player.
+
+func main() {
+
+	// Create a few players.
+
+	// Display the batting average for each player.
+
+	// Why did I not choose this form?
+
+}
