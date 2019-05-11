656fcbe47513d0d41ad457c57a5efb40f65eb071
smalldog124 <panumars124@gmail.com>
2019-05-11 16:49:58 +0700

    [Created]:สร้าง exercises ของ slices

---
+++ slices/exercises/exercises.go
@@ -0,0 +1,21 @@
+package main
+
+// Add imports.
+
+func main() {
+
+	// Declare a nil slice of integers.
+
+	// Append numbers to the slice.
+
+	// Display each value in the slice.
+
+	// Declare a slice of strings and populate the slice with names.
+
+	// Display each index position and slice value.
+
+	// Take a slice of index 1 and 2 of the slice of strings.
+
+	// Display each index position and slice values for the new slice.
+
+}
