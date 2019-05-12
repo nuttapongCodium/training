ef8a755539c1e2a4c96c83aefefc83e717af20da
platylm <n_nareenart@kkumail.com>
2019-05-12 09:43:51 +0700

    [Edited]: ลบคอมเม้นออก ของ function , method

--- method/example1/example1.go
+++ method/example1/example1.go
@@ -1,6 +1,3 @@
-// All material is licensed under the Apache License Version 2.0, January 2004
-// http://www.apache.org/licenses/LICENSE-2.0
-
 // Sample program to show how to declare methods and how the Go
 // compiler supports them.
 package main
